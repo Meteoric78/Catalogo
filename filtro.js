a8a973a0cd55aac2b661c1e722b1e16f9c95c99e
Meteoric78 <edvanfilho39@gmail.com>
2020-08-23 23:33:23 -0300

    Initial commit

---
+++ filtro.js
@@ -0,0 +1,31 @@
+// Usar objetos para inserir caracteristicas nas variaveis
+// template signo:hair-color:age:anime:fav:
+var miku = {
+    signo: "Virgem",
+    haircolor: "azul",
+    age: "16",
+    anime: "outros",
+    fav: "sim"
+  };
+  var senko = {
+    signo: "?",
+    haircolor: "laranja",
+    age: "muitos",
+    anime: "Sewayaki Kitsune no Senko san",
+    fav: "sim"
+  };
+  var megumin = {
+    signo: "sagitário",
+    haircolor: "preto",
+    age: "17",
+    anime: "Konosuba",
+    fav: "não"
+  };
+  var Elizabeth = {
+    signo: "",
+    haircolor: "branco",
+    age: "",
+    anime: "Nanatsu no taizai",
+    fav: "não"
+  };
+  console.log(megumin.haircolor);
---
+++ filtrotres.js
@@ -0,0 +1,9 @@
+var signos = {
+    Miku: "virgem",
+    Megumin: "sagitário",
+    Elizabeth: "",
+    Senko: "?",
+    
+
+}
+console.log(signos.Senko)
